5d4000cbf43ffc2a159520cfde544171484ded63
Abhi <abhi.barbiyal@cogoport.com>
2022-06-09 15:38:15 +0530

    abhi change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#index"
   # get '/about',  to: 'pages#about'
-  post '/show', to: 'pages#show'
-  post '/show/:id', to: 'pages#show'
+  # post '/show', to: 'pages#show'
+  # post '/show/:id', to: 'pag /es#show'
   post '/delete/:id' => 'pages#delete'
 end
